c41976f561449a8d2cd296007358efbb6ed0960f
aratikumbhar <aratikumbhar43@gmail.com>
2022-12-08 13:31:32 +0530

    variable declaration and initialization in first page
    variable declaration and initialization in first page

---
+++ 01_variable.js
@@ -0,0 +1,9 @@
+var myName;   //variable declaration
+myName="Arati Kumbhar";   //variable initialization
+console.log(myName);
+
+var myFrdName="kirti Sargar"   //variable declaration and initialization at the same time
+console.log(myFrdName);
+
+var myFrdName="MS Dhoni";
+console.log(myFrdName);
---
+++ 01_variable_assignment.js
@@ -0,0 +1,18 @@
+var myName;
+myName = "Arati Kumbhar";
+console.log(myName); 
+
+myFullName ="Arati Anil Kumbhar";
+console.log(myFullName);
+
+var myLove ="I Love JavaScript";
+console.log(myLove);
+
+var firstName ="Arati";
+var lastName ="Kumbhar";
+var age ="22";
+var address ="Sangola";
+var pin_code ="413307";
+var gender ="Female";
+var is_married ="No";
+console.log(firstName,lastName,age,address,pin_code,gender,is_married);
