48ed45dec577aa00ab179a1a96ec66a42770c86a
aratikumbhar <aratikumbhar43@gmail.com>
2022-12-09 19:40:20 +0530

    swap variable and typeOf variable page
    swap variable and typeOf variable page

---
+++ 01_typeOf.js
@@ -0,0 +1,23 @@
+var bankName ="SBI Bank";
+console.log(bankName);
+console.log(typeof "SBI Bank");
+
+console.log("===================================================================================");
+var marks = '90%';
+console.log(marks);
+console.log(typeof "90%");
+
+console.log("===================================================================================");
+var isWorking = "Is Working:true";
+console.log(isWorking);
+console.log(typeof true);
+
+console.log("===================================================================================");
+var totalCount ="Hundred and Seven";
+console.log(totalCount);
+console.log(typeof "Hundred and Seven");
+
+console.log("===================================================================================");
+var totalCount ="107";
+console.log(totalCount);
+console.log(typeof "107");
