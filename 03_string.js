a9b43d48066a0920b90a717a082fd2561dc8107e
aratikumbhar <aratikumbhar43@gmail.com>
2022-12-12 09:12:20 +0530

    string assignments file
    string assignments file

---
+++ 03_string.js
@@ -0,0 +1,6 @@
+var greet = "Good Morning";
+console.log("Given string is " ,greet);
+
+console.log("Total character in the string is :",greet.length);
+
+console.log("Character at index 3: ", greet.charAt(3));
