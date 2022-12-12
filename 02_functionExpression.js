aa2e2bfbc46cae8fa1901d9c097d30d5e5268133
aratikumbhar <aratikumbhar43@gmail.com>
2022-12-12 12:06:50 +0530

    modified function expression files
    modified function assignments files

--- 02_functionExpression.js
+++ 02_functionExpression.js
@@ -2,22 +2,22 @@ var myName = "Mohit";
 console.log(myName);
 
 // Function Expression
-var showResult = function() {
-    console.log("show.....");
+var showResult = function () {
+  console.log("show.....");
 };
 showResult();
 
 // We can store a function as value in JS
-var substraction = function(num1, num2){
-    var result = num1 - num2;
-    console.log("substraction.......", result);
-    return result;
-}
+var substraction = function (num1, num2) {
+  var result = num1 - num2;
+  console.log("substraction.......", result);
+  return result;
+};
 var result = substraction(45, 20);
 console.log(result);
 
-var square=function(num){
-console.log("Square of",num,"=",num*num);
-}
+var square = function (num) {
+  console.log("Square of", num, "=", num * num);
+};
 console.log(typeof square);
 square(5);
