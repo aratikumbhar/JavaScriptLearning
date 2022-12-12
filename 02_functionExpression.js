48c4f63bc1928c6473d1596ddbca067ff19ed4b7
aratikumbhar <aratikumbhar43@gmail.com>
2022-12-12 09:11:41 +0530

    function expression page
    function expression page

---
+++ 02_functionExpression.js
@@ -0,0 +1,23 @@
+var myName = "Mohit";
+console.log(myName);
+
+// Function Expression
+var showResult = function() {
+    console.log("show.....");
+};
+showResult();
+
+// We can store a function as value in JS
+var substraction = function(num1, num2){
+    var result = num1 - num2;
+    console.log("substraction.......", result);
+    return result;
+}
+var result = substraction(45, 20);
+console.log(result);
+
+var square=function(num){
+console.log("Square of",num,"=",num*num);
+}
+console.log(typeof square);
+square(5);
