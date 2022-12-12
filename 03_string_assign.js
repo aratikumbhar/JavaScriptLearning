982f68f0042936f7dab2370d0fce96b2fbe04e5b
aratikumbhar <aratikumbhar43@gmail.com>
2022-12-12 12:08:19 +0530

    modified string files
    modified string files

--- 03_string_assign.js
+++ 03_string_assign.js
@@ -7,5 +7,4 @@ console.log("Char at index 3: ", firstName.charAt(3));
 
 var lengthTotal = firstName.length;
 
-console.log("Char at last index : ", firstName.charAt(lengthTotal-1));
-
+console.log("Char at last index : ", firstName.charAt(lengthTotal - 1));
