31f130bf27349408f386aaf02ab09573324904e5
aratikumbhar <aratikumbhar43@gmail.com>
2022-12-13 18:14:49 +0530

    modified string assignment files
    modified string assignment files

---
+++ 03_string_assign1.js
@@ -0,0 +1,18 @@
+console.log("1)");
+console.log(`My dream company is : "Tata Consultancy Services"`);
+
+console.log(
+  "=============================================================================================="
+);
+console.log("2)");
+var myHobby1 = "Listening Music ";
+var myHobby2 = "Reading Books ";
+var myHobby3 = "Watching Movies";
+console.log(`My hobbies are : ${myHobby1},${myHobby2},${myHobby3}`); //using variable substitution
+console.log("My hobbies are : " + myHobby1 + myHobby2 + myHobby3); //using string template
+
+function stringTemplate() {}
+stringTemplate();
+console.log(
+  "=============================================================================================="
+);
