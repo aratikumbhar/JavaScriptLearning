e8815e1c865d09892f46cba5c54203f85b5034d7
aratikumbhar <aratikumbhar43@gmail.com>
2022-12-16 19:24:28 +0530

    if block file
    if block file

---
+++ 05_ifBlock.js
@@ -0,0 +1,26 @@
+console.log(`===== if block =====`);
+console.log(`Check whether number is positive or negative...`);
+var myNumber = 10;
+if(myNumber > 0){
+console.log(`Given number ${myNumber} is Positive`);
+}
+
+var num = -4;
+if(num <= 0){
+    console.log(`Given number ${num} is Negative`);
+}
+
+var num1 = 100;
+var num2 = 200;
+var num3 = 99;
+if(num1>num2 && num1>num3) {
+    console.log(`You are greater man ${num1}`);
+} 
+
+if(num2>num1 && num2>num3) {
+    console.log(`You are greater man ${num2}`);
+}
+
+if(num3>num1 && num3>num2) {
+    console.log(`You are greater man ${num3}`);
+}
