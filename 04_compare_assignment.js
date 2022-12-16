2319a1dfa58554721be5c648c15099f0dfd1271d
aratikumbhar <aratikumbhar43@gmail.com>
2022-12-16 19:22:56 +0530

    compare assignment file
    compare assignment file

---
+++ 04_compare_assignment.js
@@ -0,0 +1,13 @@
+console.log(`  (0 == ' ')  is`, true);
+
+console.log(`  (0 == '0')  is`, true);
+
+console.log(`  (0 == false)  is`, true);
+
+console.log(`  (null == undefined)  is`, true);
+
+console.log(`  (1 == [1])  is`, true);
+
+console.log(`  (1 == true)  is`, true);
+
+console.log(`  (1 == '1')  is`, true);
