66007b8a8304a6e4e35fc16401a94d269d215085
aratikumbhar <aratikumbhar43@gmail.com>
2022-12-17 19:38:24 +0530

    modified if block assignment file
    modified if block assignment file

---
+++ 05_ifBlock_assign.js
@@ -0,0 +1,69 @@
+console.log(`Check whether eligible for voting or not  `);
+var age = 45;
+if(age){
+console.log(`   Your age is ${age}:    You are eligible for vote`);
+}
+var age = 17;
+if(age){
+console.log(`   Your age is ${age}:    You are not eligible for vote`);
+}
+var age = 8;
+if(age){
+console.log(`   Your age is ${age}:    You are not eligible for vote`);
+}
+var age = 20;
+if(age){
+console.log(`   Your age is ${age}:    You are eligible for vote`);
+}
+var age =  -10;
+if(age){
+console.log(`   Your age is ${age}:    Invalid Data`);
+}
+var age = 200;
+if(age){
+console.log(`   Your age is ${age}:    Invalid Data`);
+}
+var age = 0;
+if(age){
+console.log(`   Your age is ${age}:    Invalid Data`);
+}
+console.log(`..........................................................................`);
+
+
+console.log(`Grade Calculation`);
+var marks = 98;
+if(marks){
+    console.log(`   Fantastic marks ${marks} :    Your grade is A+`);
+}
+var marks = 80;
+if(marks){
+    console.log(`   Excellent marks ${marks} :    Your grade is A`);
+}
+var marks = 90;
+if(marks){
+    console.log(`   Fantastic marks ${marks} :    Your grade is A`);
+}
+var marks = 0;
+if(marks){
+    console.log(`   Marks are ${marks} :  Please provide the valid marks`);
+}
+var marks = 150;
+if(marks){
+    console.log(`   Marks are ${marks} :  Please provide the valid marks`);
+}
+var marks = -7;
+if(marks){
+    console.log(`   Marks are ${marks} :  Please provide the valid marks`);
+}
+var marks = 35;
+if(marks){
+    console.log(`   Marks are ${marks} :  Your Grade is C. Need improvement`);
+}
+var marks = 64;
+if(marks){
+    console.log(`   Good Marks  ${marks} :  Your Grade is B`);
+}
+var marks = 49;
+if(marks){
+    console.log(`   Marks are ${marks} :  Your Grade is C. Need improvement`);
+}
