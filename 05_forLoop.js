f61e15fad5598f7408a7483fdd428ffbdd972224
aratikumbhar <aratikumbhar43@gmail.com>
2022-12-20 14:47:00 +0530

    for loop file
    for loop file

---
+++ 05_forLoop.js
@@ -0,0 +1,12 @@
+// Print the numbers from 2 to 20
+
+for (let index = 2; index < 10; index++) {
+   console.log(index);   
+}
+
+
+// 50 45 40 ---- 5s
+console.log(`===================================`);
+for (let index = 50; index >= 5; index=index-5) {
+    console.log(index);   
+ }
