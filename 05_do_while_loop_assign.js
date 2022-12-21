55d8927d9d0ca32e1e2e234aeb5781fe6323705f
aratikumbhar <aratikumbhar43@gmail.com>
2022-12-21 20:31:57 +0530

    do while loop assignment
    do while loop assignment

---
+++ 05_do_while_loop_assign.js
@@ -0,0 +1,56 @@
+// WAP to print numbers from 5 to 15 by incrementing 1
+console.log(`====Question 1====`);
+var i = 5;
+do {
+    console.log(i);
+    i = i+1;
+} while (i < 15);
+
+// WAP to print numbers from 50 to 40 by decrementing by 1
+console.log(`====Question 2====`);
+var i = 50;
+do {
+    console.log(i);
+    i = i-1;
+} while (i >= 40);
+
+// WAP to find first 15 odd numbers
+console.log(`====Question 3====`);
+var i = 1; 
+do {
+    console.log(i);
+    i = i+2;
+} while (i <= 15);
+
+// WAP to find 10 even numbers
+console.log(`====Question 4====`);
+var i = 2;
+do {
+    console.log(i);
+    i = i+2;
+} while (i <= 20);
+ 
+// WAP to print table of 5 like -->5 10 15 20 25 ..... 50
+console.log(`====Question 5====`);
+var i = 5;
+do {
+    console.log(i);
+    i = i+5;
+} while (i <= 50);
+
+// WAP to print table of 0 like 10 20 30 40 .... 100
+console.log(`====Question 6====`);
+var i = 10;
+do {
+    console.log(i);
+    i = i+10;
+} while (i <= 100);
+
+// WAP to print table of 10 in reverse order like --> 100 90 80 70 .... 10
+console.log(`====Question 7====`);
+var i = 100;
+do {
+    console.log(i);
+    i = i-10;
+} while (i >= 10);
+ 
