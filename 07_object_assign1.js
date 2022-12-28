4faa3264d08162cbe45f1e4706a820ba4a965564
aratikumbhar <aratikumbhar43@gmail.com>
2022-12-28 20:41:06 +0530

    modified object file
    modified object file

--- 07_object_assign1.js
+++ 07_object_assign1.js
@@ -1,70 +1,61 @@
 const professor = {
-    name: "Dhanaji Bhanvse",
-    age: 53,
-    height: 6,
-    place: "Sangola",
-    degrees: {
-      Enginerring : "Computer Science ",
-      PHD : "Java ",
-      Master : "Programming",
-    
-      certificates: {
-        certificates1: "Hacker Rank Participation",
-        certificates2: " IFE Cource",
-        certificates3: " Adv Programing",
-      },
-        // degrees: function () {
-        //   return this.Enginerring + this.PHD + this.Master 
-        // }
-      }
-   }
-   console.log(`1) All the properties: `);
-  console.log(professor);
-  console.log(``);
 
-  console.log(`2) Include nested object "Degrees":`);
-  console.log(professor.degrees);
-  // professor.degrees={
-  //   Enginerring : "Computer Science ",
-  //   PHD : "Java ",
-  //   Master : "Programming",
-  // }
-  // console.log(this);
-  // console.log(professor.degrees);
-  console.log(``);
+}
+professor.name = "Dhanaji Bhanvse",
+professor.age = 45,
+professor.gender = "Male",
+professor.city = "Sangola";
+console.log(`1) All the properties: `);
+console.log(professor);
+console.log(``);
 
-  console.log(`3) Include nested object "Certificates": `);
-  console.log(professor.degrees.certificates);
-  console.log(``);
+console.log(`2) Include nested object "Degrees":`);
+professor.degrees = {
+    Enginerring : "Computer Engineering",
+    PHD : "Adv Computing",
+    College : "VMS Sangola"
+}
+console.log(professor.degrees);
+console.log(``);
 
-  console.log(`4) Add function which concate all the Degrees:`);
-  const professorDegrees = professor.degrees;
- // console.log(`${professor.degrees}`);
-  console.log(professorDegrees);
-  console.log(``);
+console.log(`3) Include nested object "Certificates": `);
+professor.certificates = {
+    1 : "Hacker Rank",
+    2 : "Certificate In IFE",
+    3 : "Adv Programming"
+}
+console.log(professor.certificates);
+console.log(``);
 
-  professor.gender = "Male";
-  console.log(`5) Try to add new property: ${professor.gender}`);
-  console.log(professor);
-  console.log(``);
+console.log(`4) Add function which concate all the Degrees:`);
+console.log(professor.degrees);
+console.log(`The total degrees of professor are Engineering,PHD,College`);
+console.log(``);
 
-  professor.age = 50;
-  console.log("6) Modify existing property:");
- // console.log(professor.age);
-  console.log(professor);
-  console.log(`Age is modified`);
-  console.log(``);
+console.log(`5) Try to add new property: ${professor.gender}`);
+professor.height = 6;
+console.log(professor);
+console.log(``);
 
-  console.log(`7) Delete any one certificate:`);
-  delete professor.degrees.certificates.certificates1;
-  console.log(professor.degrees.certificates);
-  console.log(`Certificates1 is deleted`);
-  console.log(``);
+console.log("6) Modify existing property:");
+professor.age = 50;
+console.log(professor);
+console.log(``);
+
+console.log(`7) Delete any one certificate:`);
+delete professor.certificates[1];
+console.log(professor.certificates);
+console.log(``);
 
 console.log(`8) Add new certificate in nested object "Certificates": `);
- professor.degrees.certificates.certificates4 = "MS-CIT";
-console.log(professor.degrees.certificates);
+professor.certificates = {
+    1 : "Data Science",
+    2 : "Certificate In IFE",
+    3 : "Adv Programming"
+}
+console.log(professor.certificates);
 console.log(``);
 
 console.log(`9) Log the nested object certificate on console:`);
-console.log(professor.degrees.certificates);
+console.log(professor.certificates[1]);
+console.log(professor.certificates);
